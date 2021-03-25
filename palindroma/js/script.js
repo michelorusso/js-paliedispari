09c236c079b8147250ff5bbda3f6674aa72957d0
Michele Lorusso <kirito102095@hotmail.it>
2021-03-25 15:05:02 +0100

    Cosnegna Esercizio Palindromo

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,3 +1,41 @@
 // Palindroma
 // Chiedere all’utente di inserire una parola 
 // Creare una funzione per capire se la parola inserita è palindroma
+
+// chiediamo all'utente di inserire una parola
+var username = prompt('Dimmi il tuo nome');
+console.log(username);
+
+
+document.getElementById('yourname').innerHTML = 'Il tuo nome è : ' + username;
+
+palindrome(username);
+
+// document.getElementById('palindrome').innerHTML = 'Il tuo nome è : ' + username;
+
+
+// creiamo una funzione per capire se la parola inserita è palindroma
+function palindrome(namePalindrome) {
+
+    for( var i = 0; i < namePalindrome.length; i++) {
+
+        var readingLeft = namePalindrome[i];
+        console.log(readingLeft);
+
+    }
+
+    for( var j = namePalindrome.length - 1; j >= 0; j--) {
+
+        var readingRight = namePalindrome[j];
+        console.log(readingRight);
+    }
+
+    if(readingLeft === readingRight) {
+        document.getElementById('palindrome').innerHTML = 'il tuo nome è palindromo';
+    } else {
+        document.getElementById('palindrome').innerHTML = 'il tuo nome non è palindromo';
+    }
+
+    return namePalindrome;
+}
+
