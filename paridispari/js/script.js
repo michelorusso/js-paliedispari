72f964d6c621a6122dd1f94ca97af7898dc7a0c1
Michele Lorusso <kirito102095@hotmail.it>
2021-03-25 19:39:39 +0100

    Prima parte paridispari

--- paridispari/js/script.js
+++ paridispari/js/script.js
@@ -9,15 +9,24 @@ var choicePariDispari = prompt('Pari o Dispari?');
 console.log(choicePariDispari);
 
 // facciamo inserire all'utente un numero da 1 a 5
-var numberUser = parseInt( prompt('Scegli un numero da 1 a 5') );
-console.log(numberUser);
+var numberSelect = parseInt( prompt('Scegli un numero da 1 a 5') );
+console.log('Il tuo numero è : ' , numberSelect);
 
 // generiamo un numero random da 1 a 5 per il computer usando una funzione
 function numberRandom(computerNumber) {
-    var numberComputer = Math.floor(Math.random(1 , 5));
+
+    var numberComputer = Math.floor(Math.random() * 5) + 1;
+    console.log('Il numero del tuo avversario è : ', numberComputer);
+
+    return numberComputer;
 }
 
+var computer;
+numberRandom(computer);
+console.log(computer);
+
 // sommiamo i due numeri 
+
 
 // stabiliamo se la somma dei due numeri è pari o dispari(usando una funzione)
 
